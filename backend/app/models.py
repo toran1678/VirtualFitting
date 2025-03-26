876dcb8d082066ace2683017a5e1a93c14b7d012
seonbin <toran16784@gmail.com>
2025-03-27 06:52:26 +0900

    init

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+from database import Base # Base 클래스 import
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "items" # 테이블 이름
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), index=True)  # VARCHAR(255)로 수정
-    description = Column(String(255))  # VARCHAR(255)로 수정
+    id = Column(Integer, primary_key=True, index=True) # INTEGER, PRIMARY KEY
+    name = Column(String(255), index=True) # VARCHAR(255)
+    description = Column(String(255)) # VARCHAR(255)
---
+++ frontend/src/pages/test.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Test() {
+  return (
+    <div>
+      <h1>test page</h1>
+    </div>
+  );
+}
+
+export default Test;
